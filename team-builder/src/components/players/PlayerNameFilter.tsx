8e094ef4ff0036d250ed4480c093be3c663f3963
Jan Chen <jan_201@hotmail.de>
2025-02-02 22:19:33 +0100

    namefilter as modal

--- team-builder/src/components/players/PlayerNameFilter.tsx
+++ team-builder/src/components/players/PlayerNameFilter.tsx
@@ -1,16 +1,42 @@
-import { ChangeEvent, FunctionComponent } from "react";
+import { ChangeEvent, FunctionComponent, useRef, useState } from "react";
 import { useAppSelector } from "../../store/store";
+import { Modal } from "../shared/Modal";
+import { FilterAction } from "../layout/FilterAction";
 
-export const PlayerNameFilter: FunctionComponent<{onFilterChange: (name: string) => void}> = ({onFilterChange}) => {
+export const PlayerNameFilter: FunctionComponent<{ onFilterChange: (name: string) => void }> = ({ onFilterChange }) => {
     const nameFilter = useAppSelector((state) => state.players.queryFilter.name);
+    const [filter, setFilter] = useState(nameFilter);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const confirmRef = useRef<any>()
 
     const onInput = (e: ChangeEvent<HTMLInputElement>) => {
-        onFilterChange(e.target.value);
+        setFilter(e.target.value);
     }
 
-    if (!nameFilter) {
-        return <input placeholder="Player Name" className="w-full" onChange={onInput} />
+    const keyDown = (e: { keyCode: number; }) => {
+        if (e.keyCode !== 13) {
+            return;
+        }
+        confirmRef.current?.click();
     }
 
-    return <input value={nameFilter} className="w-full" placeholder="Player Name" onChange={onInput} />
+    return <Modal buttonContent={<FilterAction>Namefilter{nameFilter ? ': ' + nameFilter : ''}</FilterAction>}>
+        {
+            nameFilter
+                ?
+                <input autoFocus value={filter} className="w-full" placeholder="Player Name" onChange={onInput} onKeyDown={keyDown} />
+                :
+                <input autoFocus placeholder="Player Name" className="w-full" onChange={onInput} onKeyDown={keyDown} />
+        }
+        <div className="flex flex-row w-full justify-between">
+            <div className="closes-modal button" onClick={() => onFilterChange('')}>
+                Reset
+            </div>
+            <div ref={confirmRef} className="closes-modal button" onClick={() => onFilterChange(filter ?? '')}>
+                Ok
+            </div>
+        </div>
+    </Modal>
+
 }
