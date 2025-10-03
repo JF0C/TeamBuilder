d9cd5164ca7b99825ec7933bc94f50e71b7193ff
Jan Chen <jan_201@hotmail.de>
2025-10-03 16:33:26 +0200

    restore update on uncompleted matches only

--- TeamBuilder.Data/Repositories/MatchRepository.cs
+++ TeamBuilder.Data/Repositories/MatchRepository.cs
@@ -111,6 +111,11 @@ internal class MatchRepository(TeamBuilderDbContext context, IMapper mapper) : I
             .FirstOrDefaultAsync(x => x.Id == match.Id)
             ?? throw new ItemNotFoundException(match.Id.ToString(), typeof(MatchEntity));
 
+        if (matchEntity.Teams.Any(t => t.Score != 0))
+        {
+            throw new MatchCompletedException();
+        }
+
         await DeleteAsync(match.Id);
         var id = await CreateAsync(match);
         return await GetAsync(id);
